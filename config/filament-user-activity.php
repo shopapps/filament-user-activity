33a4f7a4d7064bcbc11b73f9913ba6164d4aa955
Paul <paul@shopapps.co.uk>
2025-01-23 10:32:26 +0000

    Update filament-user-activity.php
    added permissions in config

--- config/filament-user-activity.php
+++ config/filament-user-activity.php
@@ -17,4 +17,14 @@ return [
             ],
         ],
     ],
+    'permissions' => [
+        'spatie' => [
+            'active' => env('FILAMENT_USER_ACTIVITY_SPATIE_PERMISSIONS_ACTIVE', false),
+            'permission' => env('FILAMENT_USER_ACTIVITY_SPATIE_PERMISSION', 'filament_user_activity.access'),
+        ],
+        'allowed' => [
+            'emails' => array_map('trim', explode(',', env('FILAMENT_USER_ACTIVITY_ALLOWED_EMAILS', '')) ?? []) // string of comma delimited emails e.g. 'user1@test.com,user2@test.com'
+            'user_ids' => array_map('trim', explode(',', env('FILAMENT_USER_ACTIVITY_ALLOWED_USER_IDS', '')) ?? []) // string of comma delimited user ids e.g. '1,2,3'
+        ],
+    ]
 ];
